fe733336c1ea82a5a3640d93b73298ee4f08b150
Roshrosh756 <roshnisah543@gmail.com>
2026-07-12 22:47:10 +0545

    final changes

--- app.py
+++ app.py
@@ -60,5 +60,10 @@ def session_status():
     })
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
